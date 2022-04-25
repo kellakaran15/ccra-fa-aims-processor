92b56d3855117836805dada14e3c68945fe06f38
Karan Kella <karan.kella@reason.com.au>
2022-04-25 14:42:57 +1000

    First Commit

---
+++ attachFIDToCase/index.js
@@ -0,0 +1,24 @@
+const MongoClient = require('../api/dbConnect');
+const { config } = require('../config');
+const { ObjectId } = require('mongodb')
+
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const mongoClient = await MongoClient;
+    const db = await mongoClient.db(config.MONGO_DB);
+    const collection = db.collection('caseHeader');
+
+    const FIDinfo = req.body.FIDInfo;
+
+
+    const FIDInformation = await collection.updateOne({ id: req.body.caseId }, { $set: { "FIDInfo": FIDinfo } });
+
+    const responseMessage = FIDInformation;
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    config: {
+        MONGO_URI: 'mongodb://ccra-db-case-mongo:6JLXInXinbYgcHBxKnauvyLyHLpWUWlyS2EQdl4hNkmV7yXJQPbVWEyAhImjGg0MhMnZfA4EiSmh7ta7QE4LwQ==@ccra-db-case-mongo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ccra-db-case-mongo@',
+        MONGO_DB: 'MongoDB'
+    }
+};
