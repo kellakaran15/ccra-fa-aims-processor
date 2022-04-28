ccc3a5e74cac34a4dc8af8cbd1a1648145d85811
Karan Kella <karan.kella@reason.com.au>
2022-04-29 08:59:10 +1000

    change in update case status

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
+// module.exports = {
+//     config: {
+//         MONGO_URI: 'mongodb://ccra-db-case-mongo:6JLXInXinbYgcHBxKnauvyLyHLpWUWlyS2EQdl4hNkmV7yXJQPbVWEyAhImjGg0MhMnZfA4EiSmh7ta7QE4LwQ==@ccra-db-case-mongo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ccra-db-case-mongo@',
+//         MONGO_DB: 'MongoDB'
+//     }
+// };
+
+
 module.exports = {
     config: {
-        MONGO_URI: 'mongodb://ccra-db-case-mongo:6JLXInXinbYgcHBxKnauvyLyHLpWUWlyS2EQdl4hNkmV7yXJQPbVWEyAhImjGg0MhMnZfA4EiSmh7ta7QE4LwQ==@ccra-db-case-mongo.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ccra-db-case-mongo@',
+        MONGO_URI: 'mongodb://ril-case-mongodb:bJfdU4BB4T8YaO9zT6pdbXJN34eg3AXleIIGb12l4xORCQuqtUsM1fI5u3Bih8Lv57P3Q2pAHcNtc2wgpashOQ==@ril-case-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@ril-case-mongodb@',
         MONGO_DB: 'MongoDB'
     }
 };
--- updateCaseStatus/index.js
+++ updateCaseStatus/index.js
@@ -4,15 +4,32 @@ const { config } = require('../config');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const client = await MongoClient;
-    const db = await client.db(config.MONGO_DB);
-    const collection = db.collection('ccraCase');
+    const mongoClient = await MongoClient;
+    const db = await mongoClient.db(config.MONGO_DB);
+    const collection = db.collection('caseHeader');
 
-    const taskList = await collection.updateOne({ "caseDetails.caseId": req.body.id }, { $set: { "aimsData.0.fidStatus": req.body.status } });
-    const taskList2 = await collection.updateOne({ "caseDetails.caseId": req.body.id }, { $set: { "aimsData.0.fidNotes": req.body.notes } });
+    const taskList = await collection.updateOne({ id: req.body.caseId }, { $set: { "aimsData.0.fidStatus": req.body.status } });
+    const taskList2 = await collection.updateOne({ id: req.body.caseId }, { $set: { "aimsData.0.fidNotes": req.body.notes } });
     responseMessage = taskList;
 
     context.res = {
         body: responseMessage
     };
 }
+
+
+// module.exports = async function (context, req) {
+//     context.log('JavaScript HTTP trigger function processed a request.');
+
+//     const client = await MongoClient;
+//     const db = await client.db(config.MONGO_DB);
+//     const collection = db.collection('ccraCase');
+
+//     const taskList = await collection.updateOne({ "caseDetails.caseId": req.body.id }, { $set: { "aimsData.0.fidStatus": req.body.status } });
+//     const taskList2 = await collection.updateOne({ "caseDetails.caseId": req.body.id }, { $set: { "aimsData.0.fidNotes": req.body.notes } });
+//     responseMessage = taskList;
+
+//     context.res = {
+//         body: responseMessage
+//     };
+// }
