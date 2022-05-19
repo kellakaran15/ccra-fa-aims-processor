e8eb5fc61b1a2302d9d5b1ce5a8bb40372036675
Karan Kella <karan.kella@reason.com.au>
2022-05-19 20:28:37 +1000

    Attach AIMS to Case

--- attachFIDToCase/index.js
+++ attachFIDToCase/index.js
@@ -10,9 +10,18 @@ module.exports = async function (context, req) {
     const db = await mongoClient.db(config.MONGO_DB);
     const collection = db.collection('ccraCase');
 
+    var aimsPayload = {
+        "version": 1,
+        "fidId": req.body.fidId,
+        "aimsStatus": req.body.aimsStatus,
+        "aimsOutcome": req.body.aimsOutcome,
+        "aimsComments": req.body.aimsComments,
+        "fidData": FIDinfo
+    };
+
     const FIDinfo = req.body.FIDInfo;
 
-    const FIDInformation = await collection.updateOne({ "caseDetails.caseId": req.body.caseId }, { $set: { "aimsData": FIDinfo } });
+    const FIDInformation = await collection.updateOne({ "caseDetails.caseId": req.body.caseId }, { $set: { "aimsData": aimsPayload } });
 
     const responseMessage = FIDInformation;
 
